3edcdb734399cdf833a0510e5daa5d047f5fe9a3
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2023-01-27 12:54:42 +0300

    implemented bubble sort

--- practice/BubbleSort.js
+++ practice/BubbleSort.js
@@ -1,5 +1,17 @@
-
-
-function BubbleSort(arr){
-    
+// [1,3,45,5,6,7]
+// [1,3,5,6,7,45]
+function BubbleSort(arr) {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr.length; y++) {
+      if (arr[y] > arr[y + 1]) {
+        var temp = arr[y];
+        // swap
+        arr[y] = arr[y + 1];
+        arr[y + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
+
+console.log(BubbleSort([1, 3, 45, 5, 6, 7]));
