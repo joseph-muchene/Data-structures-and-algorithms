dc9a6ff7fe94c066e14dcade69cabef626d456a2
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2023-01-27 13:08:11 +0300

    optimized bubble sort

--- practice/BubbleSort.js
+++ practice/BubbleSort.js
@@ -1,8 +1,9 @@
 // [1,3,45,5,6,7]
 // [1,3,5,6,7,45]
 function BubbleSort(arr) {
-  for (let x = 0; x < arr.length; x++) {
-    for (let y = 0; y < arr.length; y++) {
+  for (let x = arr.length; x > 0; x--) {
+    for (let y = 0; y < x - 1; y++) {
+      console.log(arr[y], arr[y + 1]);
       if (arr[y] > arr[y + 1]) {
         var temp = arr[y];
         // swap
