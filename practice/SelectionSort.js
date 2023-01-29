c178cfe9c9950d5848f6d75973f052eb6d89b0fd
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2023-01-29 18:57:30 +0300

    selection sort

---
+++ practice/SelectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([2, 3, 4, 5, 1, 7, 6]));
